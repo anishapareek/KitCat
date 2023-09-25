a83ffff8d8f8408ef320edd62699c17da9186697
anishapareek <pareekanisha26@gmail.com>
2023-09-25 11:02:22 +0530

    Initial Commit

---
+++ KitCat/KitCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitCatApp.swift
+//  KitCat
+//
+//  Created by Anisha Pareek on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct KitCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
